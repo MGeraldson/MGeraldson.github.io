fba3dee28c529f0a4b033810329b24a84b81a0b4
MGeraldson <91363073+MGeraldson@users.noreply.github.com>
2021-09-25 05:52:56 -0500

    Update edm242.js

--- edm234/edm242.js
+++ edm234/edm242.js
@@ -23,6 +23,34 @@ function SetContent(content) {
         elem=document.querySelector('#OfficeHours');
     } else if(content=='Contact') {
         elem=document.querySelector('#Contact');
+    } else if(content=='MidTerm') {
+        elem=document.querySelector('#MidTerm');
+    } else if(content=='FinalAssessment') {
+        elem=document.querySelector('#FinalAssessment');
+    } else if(content=='Lab1') {
+        elem=document.querySelector('#Lab1');
+    } else if(content=='Lab2') {
+        elem=document.querySelector('#Lab2');
+    } else if(content=='Lab3') {
+        elem=document.querySelector('#Lab3');
+    } else if(content=='Lab4') {
+        elem=document.querySelector('#Lab4');
+    } else if(content=='Lab5') {
+        elem=document.querySelector('#Lab5');
+    } else if(content=='Lab6') {
+        elem=document.querySelector('#Lab6');
+    } else if(content=='Lab7') {
+        elem=document.querySelector('#Lab7');
+    } else if(content=='Lab8') {
+        elem=document.querySelector('#Lab8');
+    } else if(content=='Lab9') {
+        elem=document.querySelector('#Lab9');
+    } else if(content=='Lab10') {
+        elem=document.querySelector('#Lab10');
+    } else if(content=='Assignment1') {
+        elem=document.querySelector('#Assignment1');
+    } else if(content=='Assignment2') {
+        elem=document.querySelector('#Assignment2');
     } else if(content=='Week1') {
         elem=document.querySelector('#Week1');
     } else if(content=='Week2') {
